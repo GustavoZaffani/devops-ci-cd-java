f302ac08c0ad61d471120322ab2be8257e702eb3
GustavoZaffani <g_zaffani@hotmail.com>
2024-08-25 18:37:39 -0300

    first commit

---
+++ src/test/java/br/edu/utfpr/javaci/JavaCiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.javaci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
