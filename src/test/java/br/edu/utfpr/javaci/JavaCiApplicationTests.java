694e651f0a402201ebfa6802b42d53e5b9ec926c
GustavoZaffani <g_zaffani@hotmail.com>
2024-08-28 22:32:42 -0300

    add test

--- src/test/java/br/edu/utfpr/javaci/JavaCiApplicationTests.java
+++ src/test/java/br/edu/utfpr/javaci/JavaCiApplicationTests.java
@@ -1,15 +1,30 @@
 package br.edu.utfpr.javaci;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcPrint;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc(print = MockMvcPrint.NONE)
 class JavaCiApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
-        System.out.println("Teste de contextoo!");
-
+    void contextLoads() throws Exception {
+        mockMvc.perform(get("/api/test"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(notNullValue()));
     }
-
 }
